e7464b151bcb3b717d7f8649be3df52b97ea4317
ElinkaFIT <80892020+ElinkaFIT@users.noreply.github.com>
2022-01-20 15:11:35 +0100

    Camera follow script

---
+++ src/Assets/NewScripts/CameraFollow.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+    private void FixedUpdate()
+    {
+        transform.position = target.position + offset;
+    }
+}

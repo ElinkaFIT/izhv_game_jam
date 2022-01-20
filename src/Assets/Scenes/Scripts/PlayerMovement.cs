fa0e11c2ff79bcf05a856f83336064ae7755bc67
ElinkaFIT <alena.klimecka@gmail.com>
2022-01-20 15:26:52 +0100

    add sovicka

---
+++ src/Assets/Scenes/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Rigidbody2D _body;
+    
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        _body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        _body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, _body.velocity.y);
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            _body.velocity = new Vector2(0, 7);
+        }
+
+    }
+}

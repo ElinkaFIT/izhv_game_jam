d255837e1695b310656602a252b60823d815ecac
Natalia Bubakova <nbubakov@redhat.com>
2022-01-23 22:50:00 +0100

    (almost) final game

---
+++ src/Assets/Scripts/Menu.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public static bool GameIsOn = false;
+    public GameObject StartMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(!GameIsOn)
+            Time.timeScale = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P))
+        {
+            Debug.Log("pause?");
+            PauseGame();
+        }
+
+        if (GameIsOn)
+        {
+            StartMenu.SetActive(false);
+        }
+    }
+
+    public void PauseGame()
+    {
+        StartMenu.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsOn = false;
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        StartMenu.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsOn = true;
+    }
+    public void ResumeGame()
+    {
+        StartMenu.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsOn = true;
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
